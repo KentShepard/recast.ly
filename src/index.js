35a5aa56bf01b53e5c0b13aaa6c12a8356365936
KentShepard <kentshepard7@gmail.com>
2017-09-08 20:17:42 -0400

    Set up the video list with the 5 example video list entries

--- src/index.js
+++ src/index.js
@@ -1,2 +1,17 @@
 // TODO: Render the `App` component to the DOM
 
+// index
+  // app
+    // search
+    // videoplayer
+    // videolist
+      //videolistentry
+      //videolistentry
+      //videolistentry
+      //videolistentry
+      //videolistentry
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+);
